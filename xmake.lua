cb8474782ea155f58a0c83e1587bdd725113fb2b
M0JXD <jamiedrinkell@gmail.com>
2024-12-19 19:14:30 +0000

    cleanup

--- xmake.lua
+++ xmake.lua
@@ -1,23 +1,7 @@
-target("nonlib")
-    set_kind("shared")
-    add_files("nonlib/*.C")
-    add_includedirs("nonlib")
-    add_sysincludedirs("/usr/local/include/ntk")
-    add_sysincludedirs("/usr/include/sigc++-2.0")
-    add_sysincludedirs("/usr/lib/x86_64-linux-gnu/sigc++-2.0/include")
-    
-target("FL")
-    set_kind("static")
-    add_files("FL/*.C")
-    add_includedirs("nonlib")
-    add_sysincludedirs("/usr/local/include/ntk")
-    add_sysincludedirs("/usr/include/sigc++-2.0")
-    add_sysincludedirs("/usr/lib/x86_64-linux-gnu/sigc++-2.0/include")
-    
 target("non-sequencer")
     set_kind("binary")
     add_files("src/*.C")
-    add_deps("nonlib", "FL")
+    -- add_deps("nonlib", "FL")
     
     add_defines("VERSION=2")
     -- N.B. need to not force a system path but pick it up from the build system
